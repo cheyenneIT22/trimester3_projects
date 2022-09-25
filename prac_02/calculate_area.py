f15740698deafce748ad5f5a4de69e6eb68e43c4
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2022-09-25 21:49:22 +1000

    Calculating area program

---
+++ prac_02/calculate_area.py
@@ -0,0 +1,12 @@
+"""Calculating area of rectangle"""
+
+
+def main():
+    area = rectangle_area()
+    return rectangle_area
+
+
+def rectangle_area(length, width):
+    rectangle_area = length * width
+    return rectangle_area
+main()
